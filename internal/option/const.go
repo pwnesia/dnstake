da722db5682ddb876747df1591a67f366d9150db
indigo <mr.wild17@gmail.com>
2022-01-14 13:54:55 +0300

    added function that allows to save output into file

--- internal/option/const.go
+++ internal/option/const.go
@@ -18,10 +18,12 @@ const (
   -t, --target <HOST/FILE>    Define single target host/list to check
   -c, --concurrent <i>        Set the concurrency level (default: 25)
   -s, --silent                Suppress errors and/or clean output
+  -o, --output                Define path where you want to store the tool's output
   -h, --help                  Display its help`
   examples = `
   dnstake -t (sub.)domain.tld
   dnstake -t hosts.txt
+  dnstake -t hosts.txt -o ./dnstake.out
   cat hosts.txt | dnstake
   subfinder -silent -d domain.tld | dnstake
   `
--- internal/option/options.go
+++ internal/option/options.go
@@ -2,8 +2,9 @@ package option
 
 // Options define available flag/options
 type Options struct {
-	Target      string
-	Concurrency int
-	Silent      bool
-	List        []string
+	Target      	string
+	Concurrency 	int
+	Silent      	bool
+	List        	[]string
+	Output			string
 }
