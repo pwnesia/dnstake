f60df2355060cafb7f2f82213c62205bfcd6c75f
indigo-sadland <37074372+indigo-sadland@users.noreply.github.com>
2022-01-14 05:44:58 -0800

    Update options.go

--- internal/option/options.go
+++ internal/option/options.go
@@ -2,9 +2,9 @@ package option
 
 // Options define available flag/options
 type Options struct {
-	Target      	string
-	Concurrency 	int
-	Silent      	bool
-	List        	[]string
-	Output			string
+	Target      string
+	Concurrency int
+	Silent      bool
+	List        []string
+	Output	    string
 }
